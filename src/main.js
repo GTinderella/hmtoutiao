27d5d9493bdf5c709b8ed814bff9f4724263149b
Tinderella <1260448292@qq.com>
2019-08-02 15:34:56 +0800

    tijiao

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 控制台提示信息 开发环境提示 详细一些
 Vue.config.productionTip = false
-
+// main.js职责
+// 1.导入项目需要以来的资源
+// 2.创建一个root实例
 new Vue({
   render: h => h(App)
+  // mount首页加载
 }).$mount('#app')
